9ca9e129d4ea454d14040824080bfb2caffabe67
Hakan Özçelik <ozcelikhkn94@gmail.com>
2026-06-22 13:44:21 +0300

    Added tsconfig and edited index.html

--- qwl-restaurant/tailwind.config.js
+++ qwl-restaurant/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'owl-primary': '#c8a96e',
+        'owl-dark': '#1a1a1a',
+        'owl-text': '#777777',
+        'owl-light': '#f8f5f0',
+        'owl-primary-dark': '#b8944e',
+      },
+      fontFamily: {
+        'heading': ['"Playfair Display"', 'serif'],
+        'body': ['Roboto', 'sans-serif'],
+        'cursive': ['"Herr Von Muellerhoff"', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
