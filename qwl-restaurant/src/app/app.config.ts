6409895e3544c81d43fe576f94f581bd15a512c5
Hakan Özçelik <ozcelikhkn94@gmail.com>
2026-06-22 14:01:47 +0300

    Edited app.config.ts

--- qwl-restaurant/src/app/app.config.ts
+++ qwl-restaurant/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http'; 
+import { provideAnimations} from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './core/interceptors/auth.interceptors';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [ 
+    provideRouter(routes, withComponentInputBinding(), withInMemoryScrolling({ scrollPositionRestoration: 'enabled'})),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations(),
+  ]
 };
