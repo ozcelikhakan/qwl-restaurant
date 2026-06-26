2a04a9b4a79cd7623a8853354d0bfb9155e355a1
Hakan Özçelik <ozcelikhkn94@gmail.com>
2026-06-26 18:32:11 +0300

    Initial project setup

--- backend/qwl-restaurant.API/obj/Debug/net10.0/qwl-restaurant.API.AssemblyInfo.cs
+++ backend/qwl-restaurant.API/obj/Debug/net10.0/qwl-restaurant.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("qwl-restaurant.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ef8195b317800c82078360b942fc4bf87580cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f3c86fc0e5aad1f6a5bd01dec166ce52dea887")]
 [assembly: System.Reflection.AssemblyProductAttribute("qwl-restaurant.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("qwl-restaurant.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
